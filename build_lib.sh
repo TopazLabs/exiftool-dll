940e84e49a3213e558fea51e3a7fe4aa698da1d3
Alex_Zhang <alexbrianzhang@gmail.com>
2022-09-15 22:11:01 -0500

    Replace xxd with objcopy

--- build_lib.sh
+++ build_lib.sh
@@ -5,11 +5,14 @@
 #/d/Documents/.staticperl/perl/bin/perl /d/Documents/.staticperl/perl/bin/perltidy \
 #    -b -bext=// -dac fatlib/*.pm fatlib/**/*.pm
 
-perl FatPacker.pm file hello_world.pl >packed.pl
-xxd -i packed.pl >prog.h
+#perl FatPacker.pm file hello_world.pl >packed.pl
+#wsl objcopy --input-target binary --output-target pe-x86-64 \
+#    --binary-architecture i386 packed.pl packed.obj
 
+mkdir -p build
+cd build
 cl -LD -Z7 -W3 -DEXIFTOOL_EXPORTS -DMULTIPLICITY -DPERL_IMPLICIT_SYS -DPERLDLL \
-    exiftool.c -Fo build -Iperl5/lib/CORE -link perl5/lib/CORE/perl536s.lib -nodefaultlib \
+    ../exiftool.c -I../perl5/lib/CORE -link ../packed.obj ../perl5/lib/CORE/perl536s.lib -nodefaultlib \
     oldnames.lib kernel32.lib user32.lib advapi32.lib ucrt.lib ws2_32.lib vcruntime.lib comctl32.lib libcmt.lib
-cl -Z7 -W3 -Fo build test.c exiftool.lib
-./build/test.exe
+cl -Z7 -W3 ../test.c exiftool.lib
+test.exe
--- hello_world.pl
+++ hello_world.pl
@@ -2,18 +2,5 @@ use warnings;
 use Image::ExifTool ':Public';
 
 print("ExifTool loaded!\n");
-
-my $className = 'Image::ExifTool';
-my $exifTool = new Image::ExifTool;
-
-my $outFile = 'metadata.exif';
-$exifTool->Options(Sort => 'Tag');
-$exifTool->SetNewValuesFromFile('test-coach.jpg');
-$exifTool->SetNewValue(Artist => 'CQCumbers');
-$exifTool->WriteInfo(undef, $outFile, 'EXIF');
-
-my @ioTagList;
-my $info = $exifTool->ImageInfo($outFile, \@ioTagList);
-for my $tag (@ioTagList) {
-    print "$tag : $info->{$tag}\n";
-}
+$className = 'Image::ExifTool';
+$exifTool = new Image::ExifTool;
