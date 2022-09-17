4688d71072f9c1e4b3ef616353b97d17fdaea1dd
Alexander <alexbrianzhang@gmail.com>
2022-09-17 10:22:42 -0500

    Avoid building with other perls

--- build_lib.sh
+++ build_lib.sh
@@ -1,13 +1,18 @@
-#shopt -s globstar
-#rm -rf fatlib
-#cp -r ~/Downloads/exiftool-master/lib fatlib
-#cp -r perl5/lib/* fatlib
-#/d/Documents/.staticperl/perl/bin/perl /d/Documents/.staticperl/perl/bin/perltidy \
-#    -b -bext=// -dac fatlib/*.pm fatlib/**/*.pm
+git clone -b 12.44 --depth 1 https://github.com/exiftool/exiftool.git
+git clone -b 20220613.02 --depth 1 https://github.com/perltidy/perltidy.git
+cd perltidy && ../perl5/perl.exe pm2pl -o ../perltidy.pl
+cd ..
 
-#perl FatPacker.pm file hello_world.pl >packed.pl
-#wsl objcopy --input-target binary --output-target pe-x86-64 \
-#    --binary-architecture i386 packed.pl packed.obj
+shopt -s globstar
+rm -rf fatlib
+cp -r exiftool/lib fatlib
+cp -r perl5/lib/* fatlib
+chmod -R 777 fatlib
+perl5/perl.exe perltidy.pl -b -bext=// -dac fatlib/*.pm fatlib/**/*.pm
+
+perl5/perl.exe FatPacker.pm file hello_world.pl >packed.pl
+wsl objcopy --input-target binary --output-target pe-x86-64 \
+    --binary-architecture i386 packed.pl packed.obj
 
 mkdir -p build
 cd build
--- build_perl.bat
+++ build_perl.bat
@@ -1,3 +1,3 @@
 git clone -b v5.36.0 --depth 1 https://github.com/Perl/perl5.git
 cd perl5\win32
-nmake CCTYPE=MSVC142 ALL_STATIC=define
+nmake CCTYPE=MSVC142 BUILD_STATIC=define
